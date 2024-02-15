8fff4acdc1910a66012e0ca288af60938628a244
techysiddhant <sidjain8000@gmail.com>
2024-02-15 19:07:42 +0530

    implement the flat list

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
-import { View } from '@components/Themed';
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItem';
+import { FlatList } from 'react-native';
 
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
-    </View>
+    <FlatList data={products} renderItem={
+      ({ item }) => <ProductListItem product={item} key={item.id} />
+    } numColumns={2} contentContainerStyle={{ gap: 10, padding: 10 }} columnWrapperStyle={{ gap: 10 }} />
   );
 }
 
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -9,7 +9,7 @@ type ProductListItemProps = {
 const ProductListItem = ({ product }: ProductListItemProps) => {
  return (
   <View style={styles.container}>
-   <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+   <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} resizeMode='contain' />
    <Text style={styles.title}>{product.name}</Text>
    <Text style={styles.price}>${product.price}</Text>
   </View>
@@ -21,7 +21,9 @@ const styles = StyleSheet.create({
  container: {
   backgroundColor: Colors.light.background,
   padding: 10,
-  borderRadius: 20
+  borderRadius: 20,
+  flex: 1,
+  maxWidth: '50%'
  },
  title: {
   fontSize: 18,
